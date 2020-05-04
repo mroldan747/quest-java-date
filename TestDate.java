0b3832a0a56b6b7a5c4e62f0cb13f1313344a0c7
mroldan747 <mroldan747@gmail.com>
2020-05-04 09:48:57 +0200

    dates working

--- TestDate.java
+++ TestDate.java
@@ -3,7 +3,7 @@ import java.util.Calendar;
 import java.util.GregorianCalendar;
 import java.util.Date;
 import java.util.Scanner; 
-
+import java.util.GregorianCalendar;
 class TestDate {
 
     public static void main(String[] args) {
@@ -17,7 +17,13 @@ class TestDate {
         int year = sc.nextInt();
         sc.close();
 
-        // TODO : your code after this line
+        SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy"); 
+        Calendar c = new GregorianCalendar(year, month - 1, day);
+        Date d = c.getTime();
+        System.out.println("The date is : " + sdf.format(d));
+        int dayOfWeek = c.get(Calendar.DAY_OF_WEEK);
+        System.out.println("The day of the week is : " + dayOfWeek);
+
         
     }
 }
